bf0d8ec2df996a4052d175bb46c9145dbb2e06d6
Laurent <laurent.ruaud@serli.com>
2017-03-02 08:03:53 +0100

    Implement test solutions

--- src/test/java/com/serli/dojo/trainings/fp/T01_JavaFunctions.java
+++ src/test/java/com/serli/dojo/trainings/fp/T01_JavaFunctions.java
@@ -2,6 +2,7 @@ package com.serli.dojo.trainings.fp;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Supplier;
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -33,21 +34,26 @@ public class T01_JavaFunctions {
 
     @Test
     public void E02_staticMethodReference() throws Exception {
-        Supplier<Integer> rollOne = null; //TODO: get the reference to the static method 'roll' of the class 'Dice'
+        Supplier<Integer> rollOne = Dice::roll; //TODO: get the reference to the static method 'roll' of the class 'Dice'
         assertThat(rollOne, notNullValue());
         assertThat(rollOne.get(), Matchers.isOneOf(1, 2, 3, 4, 5, 6));
     }
 
     @Test
     public void E03_innerClass() throws Exception {
-        Supplier<Integer> rollOne = null; //TODO: create an inner class to implement the method 'roll'
+        Supplier<Integer> rollOne = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return new Random().nextInt(6) + 1;
+            }
+        }; //TODO: create an inner class to implement the method 'roll'
         assertThat(rollOne, notNullValue());
         assertThat(rollOne.get(), Matchers.isOneOf(1, 2, 3, 4, 5, 6));
     }
 
     @Test
     public void E04_lambda() throws Exception {
-        Supplier<Integer> rollOne = null; //TODO: create a lambda to implement the method 'roll'
+        Supplier<Integer> rollOne = () -> new Random().nextInt(6) + 1; //TODO: create a lambda to implement the method 'roll'
         assertThat(rollOne, notNullValue());
         assertThat(rollOne.get(), Matchers.isOneOf(1, 2, 3, 4, 5, 6));
     }
